3fcd7291c04b5a3d20bb0a400e1d32d3f402eb53
christian figueiredo <developer.figueiredo@gmail.com>
2025-08-19 12:08:14 -0300

    atualizacao linq

--- src/MasterNet.Persistence/MainEntry.cs
+++ src/MasterNet.Persistence/MainEntry.cs
@@ -9,27 +9,7 @@ using var context = new MasterNetDbContext();
 var cursos = await context.Cursos!.ToListAsync();
 var instrutores = await context.Instrutores!.ToListAsync();
 
-var cursoNovo = new Curso()
-{
-    Id = Guid.NewGuid(),
-    Titulo = "Programando em C#",
-    Descricao = "Curso de C#",
-    DataPublicacao = DateTime.UtcNow
-};
 
-context.Add(cursoNovo);
-await context.SaveChangesAsync();
-
-var instrutorNovo = new Instrutor()
-{
-    Id = Guid.NewGuid(),
-    Nome = "Joao",
-    Sobrenome = "Silva",
-    GrauAcademico = "Mestre"
-};
-
-context.Add(instrutorNovo);
-await context.SaveChangesAsync();
 
 foreach (var curso in cursos)
 {
@@ -39,7 +19,7 @@ foreach (var curso in cursos)
 
 foreach (var Instrutor in instrutores)
 {
-    Console.WriteLine($"{Instrutor.Id} : {Instrutor.Nome}");
+    Console.WriteLine($"{Instrutor.Id} : {Instrutor.Nome} : {Instrutor.GrauAcademico}");
 }
 
 
